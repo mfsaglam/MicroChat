7ad9d2f6ab657d462cedc849c06a42378ca5f52f
mfsaglam <mfsaglam1@gmail.com>
2021-10-14 16:08:29 +0300

    Initial Commit

---
+++ MicroChat/MicroChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  MicroChatApp.swift
+//  MicroChat
+//
+//  Created by Fatih Sağlam on 14.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct MicroChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
