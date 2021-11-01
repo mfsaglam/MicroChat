cb73b6b8d5b748a10b3f680c6ca35bddd7cecf18
mfsaglam <mfsaglam1@gmail.com>
2021-11-01 23:11:53 +0300

    Add ConversationsView.

--- MicroChat/ConversationsView.swift
+++ MicroChat/ConversationsView.swift
@@ -8,43 +8,21 @@
 import SwiftUI
 
 struct ConversationsView: View {
-    var segments = ["Active", "Archived"]
-    @State var selectedSegment = "Active"
-    @Namespace var segmentNamespace
+    
     var body: some View {
         ZStack {
             Color(#colorLiteral(red: 0.9656175971, green: 0.9692887664, blue: 0.984508574, alpha: 1))
                 .ignoresSafeArea()
             VStack {
-                Text("Conversations")
-                    .font(.largeTitle)
-                    .bold()
-                HStack {
-                    Button {
-                        
-                    } label: {
-                        Image(systemName: "magnifyingglass")
-                            .accentColor(.primary)
-                            .opacity(0.5)
-                            .padding(15)
-                            .background(Color.white)
-                            .cornerRadius(10)
+                ConversationsControlPanel()
+                ScrollView {
+                    ForEach(0 ..< 5) { item in
+                        ConversationBubble()
+                            .padding(.horizontal, 30)
+                            .shadow(color: Color.black.opacity(0.05), radius: 20, x: 0, y: 10)
                     }
-                    HStack {
-                        ForEach(segments, id: \.self) { segment in
-                            Button {
-                                withAnimation(.interactiveSpring()) {
-                                    selectedSegment = segment
-                                }
-                            } label: {
-                                SegmentedControl(selectedSegment: $selectedSegment, namespace: segmentNamespace, text: segment)
-                                    .accentColor(.primary)
-                            }
-                        }
-                    }
-                    .background(Color.white)
-                    .cornerRadius(15)
                 }
+                .padding(.bottom)
             }
             VStack {
                 Spacer()
@@ -85,13 +63,15 @@ struct ConversationsView: View {
                 .ignoresSafeArea(edges: .bottom)
             }
         }
+        .navigationTitle("Conversations")
     }
 }
 
-
 struct ConversationsView_Previews: PreviewProvider {
     static var previews: some View {
-        ConversationsView()
+        NavigationView {
+            ConversationsView()
+        }
     }
 }
 
@@ -117,5 +97,86 @@ struct SegmentedControl: View {
                 .padding(.horizontal, 20)
         }
         
+    }
+}
+
+struct ConversationsControlPanel: View {
+    var segments = ["Active", "Archived"]
+    @State var selectedSegment = "Active"
+    @Namespace var segmentNamespace
+    var body: some View {
+        HStack {
+            Button {
+                
+            } label: {
+                Image(systemName: "magnifyingglass")
+                    .accentColor(.primary)
+                    .opacity(0.5)
+                    .padding(15)
+                    .background(Color.white)
+                    .cornerRadius(10)
+            }
+            HStack {
+                ForEach(segments, id: \.self) { segment in
+                    Button {
+                        withAnimation(.interactiveSpring()) {
+                            selectedSegment = segment
+                        }
+                    } label: {
+                        SegmentedControl(selectedSegment: $selectedSegment, namespace: segmentNamespace, text: segment)
+                            .accentColor(.primary)
+                    }
+                }
+            }
+            .background(Color.white)
+            .cornerRadius(15)
+        }
+        .padding(.top)
+    }
+}
+
+struct ConversationBubble: View {
+    var body: some View {
+        HStack {
+            VStack {
+                Image(systemName: "person.crop.circle")
+                    .resizable()
+                    .frame(width: 60, height: 60)
+                Text("1")
+                    .foregroundColor(Color(#colorLiteral(red: 0.8549019608, green: 0.2392156863, blue: 0.0862745098, alpha: 1)))
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 5)
+                    .background(
+                        Capsule()
+                            .fill(Color(#colorLiteral(red: 0.8549019608, green: 0.2392156863, blue: 0.0862745098, alpha: 1)).opacity(0.3))
+                    )
+            }
+            .padding()
+            VStack(alignment: .leading) {
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("Huo Shu")
+                            .fontWeight(.semibold)
+                        Text("1 day")
+                            .foregroundColor(Color(#colorLiteral(red: 0.3176470588, green: 0.2823529412, blue: 0.6980392157, alpha: 1)))
+                    }
+                    Spacer()
+                    Image(systemName: "checkmark")
+                        .foregroundColor(Color(#colorLiteral(red: 0.3124988973, green: 0.292444706, blue: 0.7242618203, alpha: 1)))
+                        .padding(10)
+                        .background(Color(#colorLiteral(red: 0.3124988973, green: 0.292444706, blue: 0.7242618203, alpha: 1)).opacity(0.3))
+                        .clipShape(Circle())
+                }
+                Text("Hello, I can't find my crenditial in the app can you help me with finding it because it is driving me crazy so much.")
+                    .opacity(0.5)
+                    .lineLimit(2)
+                    .padding(.top, 12)
+            }
+        }
+        .padding(20)
+        .background(
+            Color.white
+        )
+        .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
     }
 }
